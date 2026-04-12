3448fe0b9b8b8d82b2171a7d12d3bcfc452c88a6
chsrvn <chsrvn@gmail.com>
2026-04-12 19:16:00 +0530

    Initial Commit

---
+++ src/main/java/com/chsrvn/splitsoservice/SplitsoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chsrvn.splitsoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitsoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitsoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chsrvn/splitsoservice/SplitsoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chsrvn.splitsoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitsoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
